9938d47d6a50bd5b17709791e4070b84a906d2c1
jfh <jfh@jfh-laptop>
2011-02-27 09:56:40 +0100

    Added jQuery VIE editable widget.

---
+++ jquery.vie.editable.js
@@ -0,0 +1,60 @@
+/*
+ * jQuery VIE editable
+ *
+ */
+var VieEditable = {
+    options: {
+
+	},
+
+    editors: {
+        wymeditor: {
+            enableEditable: function(elem) {
+                var containerInstance = VIE.ContainerManager.getInstanceForContainer(jQuery(elem));
+			    containerInstance.editables = {};
+                jQuery(elem).find('[property]').each(function() {
+                    var containerProperty = jQuery(this);
+				    var propertyName = containerProperty.attr('property');
+				    if (containerProperty.is('div')) {
+					    containerProperty.wymeditor({
+						    html: containerProperty.html(),
+						    stylesheet: 'vie-wymeditor-stylesheet.css',
+						    postInit: function (wym) {
+							    containerInstance.editables[propertyName] = wym;
+							    wym.vieContainerInstance = containerInstance;
+							}
+				        });
+				    }
+			    });
+            },
+            disableEditable: function() {},
+            isModified: function() {},
+            getContents: function() {}
+        },
+        aloha: {
+            enableEditable: function(elem) {},
+            disableEditable: function() {},
+            isModified: function() {},
+            getContents: function() {}
+        }
+    },
+
+	_create: function() {
+	    this.editors[this.options.editor].enableEditable(this.element);
+	},
+
+	_destroy: function() {
+		
+	},
+
+	_enable: function(test) {
+
+	},
+
+	_disable: function() {
+
+	},
+
+};
+
+$.widget('vie.vieEditable', VieEditable);
