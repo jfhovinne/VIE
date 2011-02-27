108284e52c012222666aca2ea214090f46ba7f11
jfh <jfh@jfh-laptop>
2011-02-27 16:14:51 +0100

    Clean up.

--- js/jquery.vie.editable.js
+++ js/jquery.vie.editable.js
@@ -6,56 +6,56 @@ var VieEditable = {
     options: {
         editor: false,
         stylesheet: false
-	},
+    },
 
     editors: {
         wymeditor: {
-            enableEditable: function(elem) {
-                var containerInstance = VIE.ContainerManager.getInstanceForContainer(jQuery(elem));
-			    containerInstance.editables = {};
+            enableEditable: function(o) {
+                var containerInstance = VIE.ContainerManager.getInstanceForContainer(jQuery(o.element));
+                containerInstance.editables = {};
 
-                jQuery(elem).find('[property]').each(function() {
+                jQuery(o.element).find('[property]').each(function() {
                 
                     var containerProperty = jQuery(this);
-				    var propertyName = containerProperty.attr('property');
-				    if (containerProperty.is('div')) {
-					    containerProperty.wymeditor({
-						    html: containerProperty.html(),
-						    postInit: function (wym) {
-							    containerInstance.editables[propertyName] = wym;
-							    wym.vieContainerInstance = containerInstance;
-							}
-				        });
-				    }
-			    });
+                    var propertyName = containerProperty.attr('property');
+                    if (containerProperty.is('div')) {
+                        containerProperty.wymeditor({
+                            html: containerProperty.html(),
+                            postInit: function (wym) {
+                                containerInstance.editables[propertyName] = wym;
+                                wym.vieContainerInstance = containerInstance;
+                            }
+                        });
+                    }
+                });
             },
             disableEditable: function() {},
             isModified: function() {},
             getContents: function() {}
         },
         aloha: {
-            enableEditable: function(elem) {},
+            enableEditable: function(o) {},
             disableEditable: function() {},
             isModified: function() {},
             getContents: function() {}
         }
     },
 
-	_create: function() {
-	    this.editors[this.options.editor].enableEditable(this.element);
-	},
+    _create: function() {
+        this.editors[this.options.editor].enableEditable(this);
+    },
 
-	_destroy: function() {
-		
-	},
+    _destroy: function() {
+        
+    },
 
-	_enable: function(test) {
+    _enable: function(test) {
 
-	},
+    },
 
-	_disable: function() {
+    _disable: function() {
 
-	},
+    },
 
 };
 
